0d09d8164d7da5436e19b82ecc9de58099229423
YanPaiva <gckael@gmail.com>
2018-09-27 21:37:19 -0300

    Padrao Strategy

---
+++ src/padraostrategy/PadraoStrategi.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package padraostrategy;
+
+/**
+ *
+ * @author yan
+ */
+public class PadraoStrategi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Produto produto1 = new TV();
+        Produto produto2 = new Camera();
+        Produto produto3 = new Celular();
+        
+        System.out.println(produto1.getNome() +
+                "na promoção com desconto de "+produto1.desconto()+"%");
+        System.out.println(produto2.getNome() +"na promoção com desconto de "
+                +produto2.desconto()+"%");
+        System.out.println(produto3.getNome() +
+                "na promoção com desconto de "+produto3.desconto()+"%");
+        
+    }
+    
+}
---
+++ src/padraostrategy/PromocaoQueimaEstoque.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package padraostrategy;
+
+/**
+ *
+ * @author yan
+ */
+public class PromocaoQueimaEstoque implements Promocao {
+
+    @Override
+    public int obterDesconto() {
+        return 20;
+    }
+
+    @Override
+    public String obterPromocao() {
+        return "Queima de Estoque";
+    }
+}
